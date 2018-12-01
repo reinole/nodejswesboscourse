33d4811fd74cca8d5480c68699967ae055eb4a2e
Ole christian Reinhartsen <reinhartsen.oc@gmail.com>
2018-12-01 11:02:35 +0100

    Completed #13

--- my-fantastic-node-shop/controllers/storeController.js
+++ my-fantastic-node-shop/controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index');
 };
@@ -6,6 +9,19 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
+exports.createStore = async (req, res) => {
   //sends data back to user view: res.json(req.body);
+
+  const store = await new Store(req.body).save();
+  req.flash(
+    'success',
+    `Successfully created ${store.name}. Care to leave a review?`
+  );
+
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
 };
